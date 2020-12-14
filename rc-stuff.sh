20983173c00819d3bb41b803dfb8651737689d09
Fredrik Bystam <fredrik.bystam@kry.se>
2020-12-14 13:15:43 +0100

    add some color schemes

---
+++ rc-stuff.sh
@@ -0,0 +1,9 @@
+
+function git-branch-prune {
+    echo "Pruning branches..."
+    git fetch -p
+    for branch in `git branch -vv | grep ': gone]' | awk '{print $1}'`; 
+    do 
+        git branch -D $branch; 
+    done
+}
